b25dd254d4b49e416046bd4568540148d0c6c057
henrywoody <github@henrywoody.com>
2020-06-02 00:05:27 -0700

    Adds vehicle maintenance event table
    Adds the `vehicle_maintenance_event` table for recording maintenance
    perfomed on vehicles.

---
+++ migrations/003-vehicle-maintenance.sql
@@ -0,0 +1,8 @@
+CREATE TABLE vehicle_maintenance_event (
+    id SERIAL PRIMARY KEY,
+    event_type TEXT NOT NULL,
+    event_date DATE NOT NULL,
+    vehicle_id INT NOT NULL REFERENCES vehicle (id),
+    vehicle_mileage DOUBLE PRECISION NOT NULL,
+    notes TEXT
+);
